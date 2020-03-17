c132f6ccc080e6a9cf0aaa8ee565549a52d1c83d
Vladislav <zhurkin388@bk.ru>
2020-03-17 19:35:02 +0300

    17.03

---
+++ js/booking-min.js
@@ -0,0 +1,1 @@
+var search=document.querySelector(".search-btn"),formbody=document.querySelector(".form-intro");search.addEventListener("click",function(e){e.preventDefault(),formbody.classList.toggle("form-show")});
